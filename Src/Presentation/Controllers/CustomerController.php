b16cb626ebfe704ec44a98cd4d40bae6ef4f278e
ManuelFBS <manuelf.borrego@gmail.com>
2026-06-07 17:36:42 -0400

    Actualiz. 'CustomerController.php'...

--- Src/Presentation/Controllers/CustomerController.php
+++ Src/Presentation/Controllers/CustomerController.php
@@ -23,5 +23,36 @@ class CustomerController
         {
                 $customerRepository = new MySQLCustomerRepository();
                 $userRepository = new MySQLUserRepository();
+
+                $this->createCustomerUseCase =
+                        new CreateCustomerUseCase($customerRepository);
+                $this->listCustomerUseCase =
+                        new ListCustomersUseCase($customerRepository);
+                $this->getCustomerUseCase =
+                        new GetCustomerUseCase($customerRepository);
+                $this->updateCustomerUseCase =
+                        new UpdateCustomerUseCase($customerRepository);
+                $this->deleteCustomerUseCase =
+                        new DeleteCustomerUseCase($customerRepository, $userRepository);
+        }
+
+        // * Listar todos los clientes...
+        public function index(): void
+        {
+                try {
+                        $users = $this->listCustomerUseCase->execute();
+                        require_once __DIR__ . '/../Views/layouts/navbar.php';
+                        require_once __DIR__ . '/../Views/customers/index.php';
+                } catch (\Exception $e) {
+                        $_SESSION['error_message'] = $e->getMessage();
+                        header('Location: ' . $_ENV['APP_URL']);
+                }
+        }
+
+        // * Mostrar formulario de creación...
+        public function create(): void
+        {
+                require_once __DIR__ . '/../Views/layouts/navbar.php';
+                require_once __DIR__ . '/../Views/customers/index.php';
         }
 }
